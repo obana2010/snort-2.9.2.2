350cc0e2b1884cb99391ddbcc58a2c6349ba80b2
obana <yoshimasa-o@is.naist.jp>
2012-07-11 17:17:44 +0900

    first commit

---
+++ src/dynamic-examples/dynamic-preprocessor/kyoto.cpp
@@ -0,0 +1,17 @@
+#include "kyoto.h"
+#include <ktremotedb.h>
+//#include <stdio.h>
+using namespace std;
+using namespace kyototycoon;
+
+
+//int main(int, char **){
+//  return 0;
+//}
+
+extern "C"
+void extfunc(void)
+{
+printf("***** extfunc\n");
+  return;
+}
